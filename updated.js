47edbb06a23e6472d580e9503242900d90e70413
ka-wepee <127013624+ka-wepee@users.noreply.github.com>
2023-04-20 17:59:18 +0800

    Delete updated.js

--- updated.js
+++
@@ -1,8 +0,0 @@
-fetch('https://raw.githubusercontent.com/ka-wepee/touch-data/main/data.json')
-  .then(response => response.json())
-  .then(data => {
-    const updated = data[0].updated;
-    const inputElem = document.getElementById('search-item');
-    inputElem.placeholder += updated;
-  })
-  .catch(error => console.error(error));
